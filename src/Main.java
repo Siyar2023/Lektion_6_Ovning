0f2773e2ddd984df4e462239c5a5a0ea91de2efc
Roni7823 <siy_sar78@yahoo.se>
2023-09-28 10:06:49 +0200

    firs commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+//Felhantering
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {0, 100, 1000};
+        int test;
+
+       /*Scanner scanner = new Scanner(System.in);
+        System.out.println("skriv ngt för att krascha");
+        scanner.nextLine();
+        System.out.println(number/0);*/
+
+        for (int i = 0; i < numbers.length; i++);
+
+            System.out.println(test);
+
+
+    }
+}
+
